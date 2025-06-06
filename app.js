dbb33ff81f0d89240d17ba0c82ec3da187794bb9
dashpen <dashpenning@gmail.com>
2025-06-06 16:40:26 -0700

    refactored queries to another file and request code

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const express = require('express')
-const { Pool } = require('pg')
-require('dotenv').config()
-const auth =  require('./auth.js')
+import express, { json } from 'express'
+import 'dotenv/config'
+import auth from './assets/js/auth.js'
+import session from 'express-session'
+import * as query from './assets/js/query.js'
 
 const app = express()
 const port = 3000
 
-app.use(express.json()) // Middleware to parse JSON request bodies
+app.use(json()) // Middleware to parse JSON request bodies
 
-const pool = new Pool({
-  user: process.env.DB_USER_NAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 })
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 app.get('/', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM user_data')
-    res.json(result.rows)
+    const result = query.getAllUsers()
+    result.then(data => {
+      res.status(200).json(data)
+    })
   }
   catch (err) {
     console.error(err)
@@ -31,31 +38,31 @@ app.post('/signup', async (req, res) => {
   if(req.headers['content-type'] !== 'application/json') {
     return res.status(400).send('Content-Type must be application/json')
   }
-  if (!req.body) {
-    return res.status(400).send('Request body is required')
-  }
-  console.log(req)
   try {
     let { username, password, email } = req.body
-    if (!username || !password || !email) {
-      return res.status(400).send('Missing required fields')
-    }
     const validatedInput = auth.user_schema.validate({username: username, password: password, email: email})
-    if (validatedInput.error) {
-      return res.status(400).send(validatedInput.error)
-    }
-    console.log(validatedInput)
+    
+    if (validatedInput.error) return res.status(400).send(validatedInput.error.message)
+
     password = await auth.hashPassword(password)
-    if (!password) {
-      return res.status(500).send('Error hashing password')
-    }
-    const existingUser = await pool.query('SELECT * FROM user_data WHERE username = $1', [username])
-    if (existingUser.rows.length > 0) {
+
+    if (!password) return res.status(500).send('Error hashing password')
+
+    console.log(`Creating user with username: ${username}, email: ${email}`)
+
+    const existingUser = query.getUserByUsername(username)
+
+    if (existingUser) {
       return res.status(409).send('Username already exists')
     }
-    const result = await pool.query("INSERT INTO user_data (username, password, email) VALUES ($1, $2, $3) RETURNING *",
-       [username, password, email])
-    res.status(201).json(result.rows[0])
+
+    const result = query.addUser(username, password, email)
+
+    result.then(data => {
+      if (!data) return res.status(500).send('Error creating user')
+      res.status(201).json(data)
+    })
+
   }
   catch (err) {
     console.error(err)
@@ -63,6 +70,35 @@ app.post('/signup', async (req, res) => {
   }
 })
 
+
+app.get('/login', async (req, res) => {
+  if(req.headers['content-type'] !== 'application/json') {
+    return res.status(400).send('Content-Type must be application/json')
+  }
+  if (!req.body) {
+    return res.status(400).send('Request body is required')
+  }
+  try {
+    const { username, password } = req.body
+    if (!username || !password) {
+      return res.status(400).send('Missing required fields')
+    }
+    const user = await pool.query('SELECT * FROM user_data WHERE username = $1', [username])
+    if (user.rows.length === 0) {
+      return res.status(404).send('User not found')
+    }
+    const isValidPassword = await auth.verifyPassword(password, user.rows[0].password)
+    if (!isValidPassword) {
+      return res.status(401).send('Invalid password')
+    }
+    res.json({ message: 'Login successful' })
+  }
+  catch (err) {
+    console.error(err)
+    res.status(500).send('Internal Server Error')
+  }
+}
+)
 
 app.get('/problems', async (req, res) => {
   try {
